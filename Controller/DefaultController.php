55c1ce08b8fbc1d0e5c640bfd771d35d75318873
Tabernicola <tabernicola01@gmail.com>
2013-06-09 19:05:18 +0200

    Bundle creation

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tabernicola\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TabernicolaBlogBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ TabernicolaBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tabernicola\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TabernicolaBlogBundle extends Bundle
+{
+}
